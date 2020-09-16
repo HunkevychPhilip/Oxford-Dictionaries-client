c0c232bca7e61d9d8eeec3297c88283ffb3d2993
Philip Hunkevych <pandaemoniumplaza@gmail.com>
2020-09-16 22:53:57 +0300

    Update MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\System\Client\GuzzleClient;
+use App\System\v2\Dictionary;
+use App\System\v2\DictionaryException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class MainController extends AbstractController
 {
@@ -19,8 +23,28 @@ class MainController extends AbstractController
     /**
      * @Route("/search", name="app_search", methods="GET")
      */
-    public function search(): Response
+    public function index(): Response
     {
-        return $this->render('main/search.html.twig');
+        $word = 'run';
+
+        $error = null;
+        $results = [];
+
+        $dictionary = new Dictionary(new GuzzleClient());
+
+        try {
+            $results = $dictionary->entries('en-gb', $word);
+        } catch (DictionaryException $e) {
+            $error = $e->getMessage();
+        }
+
+        dump($results);
+
+
+        return $this->render('main/search.html.twig', [
+            'word' => $word,
+            'results' => $results,
+            'error' => $error,
+        ]);
     }
 }
