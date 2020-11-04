cef258a3b7d85657fe51ea86935ab2d0bd3c5d78
Philip Hunkevych <60174953+HunkevychPhilip@users.noreply.github.com>
2020-11-04 14:36:02 +0200

    Form simplifying

--- src/Form/SearchFormType.php
+++ src/Form/SearchFormType.php
@@ -18,20 +18,25 @@ class SearchFormType extends AbstractType
                     'maxlength' => 30,
                     'pattern' => "^[A-Za-z]+$",
                 ],
-            ])->add('fields', ChoiceType::class, [
-                'choices' => [
-                    'Definitions' => 'definitions',
-                    'Pronunciations' => 'pronunciations',
-                    'Examples' => 'examples',
-                ],
-                'expanded' => true,
-                'multiple' => true,
-            ])
-            ->add('language', ChoiceType::class, [
-                'choices' => [
-                    'British English' => 'en-gb',
-                    'American English' => 'en-us',
-                ]
             ]);
     }
 }
+
+
+//->add('fields', ChoiceType::class, [
+//    'choices' => [
+//        'Definitions' => 'definitions',
+//        'Pronunciations' => 'pronunciations',
+//        'Examples' => 'examples',
+//    ],
+//    'expanded' => true,
+//    'multiple' => true,
+//])
+
+
+//->add('language', ChoiceType::class, [
+//    'choices' => [
+//        'British English' => 'en-gb',
+//        'American English' => 'en-us',
+//    ]
+//]);
